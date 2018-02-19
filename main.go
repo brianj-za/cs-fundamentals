f74c6cc7b99c079a3c3e1149453b52342a917c6c
Brian Johnson <brian@brianjohnson.co.za>
2018-02-19 11:44:59 +0200

    Added vendoring.

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 	arr := r.Perm(20000)
 	arr2 := sort.Bubble(arr)
 
-	r = rand.New(rand.NewSource(99))
-	arr = r.Perm(20000)
-	arr3 := sort.Insertion(arr)
-	if len(arr) != len(arr3) {
-		panic("I broke!")
-	}
+	// r = rand.New(rand.NewSource(99))
+	// arr = r.Perm(20000)
+	// arr3 := sort.Insertion(arr)
+	// if len(arr) != len(arr3) {
+	// 	panic("I broke!")
+	// }
 
 	r = rand.New(rand.NewSource(99))
 	arr = r.Perm(20000)
@@ -28,25 +28,25 @@ func main() {
 		panic("I broke!")
 	}
 
-	r = rand.New(rand.NewSource(99))
-	arr = r.Perm(20000)
-	arr5 := sort.Quick(arr)
-	if len(arr) != len(arr5) {
-		panic("I broke!")
-	}
+	// r = rand.New(rand.NewSource(99))
+	// arr = r.Perm(20000)
+	// arr5 := sort.Quick(arr)
+	// if len(arr) != len(arr5) {
+	// 	panic("I broke!")
+	// }
 
 	// As a test, take sorted case
 	log.Println("\nSorted cases")
 	arr2 = sort.Bubble(arr2)
-	arr3 = sort.Insertion(arr3)
+	// arr3 = sort.Insertion(arr3)
 	arr4 = sort.Merge(arr4)
-	arr5 = sort.Quick(arr5)
+	// arr5 = sort.Quick(arr5)
 
 	log.Println("\nReverse Sorted cases")
 	arr2 = sort.Bubble(reverse(arr2))
-	arr3 = sort.Insertion(reverse(arr3))
+	// arr3 = sort.Insertion(reverse(arr3))
 	arr4 = sort.Merge(reverse(arr4))
-	arr5 = sort.Quick(reverse(arr5))
+	// arr5 = sort.Quick(reverse(arr5))
 }
 
 func reverse(numbers []int) []int {
